e8ad74ed1d08c8477b7a2960e9e3cb72dcb72a1b
cryptonomicon46 <sandman2145@gmail.com>
2023-01-23 23:38:54 -0800

    Wrapper Contract logic in place

--- contracts/SupeRareWrapper.sol
+++ contracts/SupeRareWrapper.sol
@@ -2,7 +2,8 @@
 pragma solidity =0.7.6;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-// import "./IERC721Addons.sol";
+
+import "./IERC721Receiver.sol";
 
 contract SupeRareWrapper is Ownable  {   
     address public immutable SupeOriginalAddr;
@@ -11,6 +12,15 @@ contract SupeRareWrapper is Ownable  {
     mapping (address => mapping (address => bool)) private _operatorApprovals;
 
     event ApprovedThisContract(uint256 tokenId);
+
+    /**
+     * @dev Emitted when `tokenId` token is transferred from `from` to `to`.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    // Equals to `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`
+    // which can be also obtained as `IERC721Receiver(0).onERC721Received.selector`
+    bytes4 private constant _ERC721_RECEIVED = 0x150b7a02;
+
     constructor (address _SupeOriginalAddr) {
         SupeOriginalAddr = _SupeOriginalAddr;
 
@@ -23,14 +33,14 @@ contract SupeRareWrapper is Ownable  {
     ///  emits a SwapCompleted event
     ///
     function safeTransferFrom(address from, address to, uint256 tokenId) public virtual override {
-        require(SupeOriginalAddr.approvedFor(tokenId) == address(this),"WRAPPER_NOT_APPROVED!" );
+       
     
         safeTransferFrom(from, to, tokenId, "");
     }
 
 
     function ApproveThisContract(uint tokenId) external view {
-        require(SupeOriginalAddr.approvedFor(tokenId)!=0,"INVALID_TOKENID");
+        require(SupeOriginalAddr.approvedFor(tokenId)!=address(0),"ERC721: operator query for nonexistent token");
         require(SupeOriginalAddr.ownerOf(tokenId)== msg.sender ||SupeOriginalAddr.approvedFor(tokenId)== msg.sender ,"NOT_OWNER_OR_APPROVED");
         SupeOriginalAddr.approve(address(this),tokenId);
         emit ApprovedThisContract(tokenId);
@@ -41,17 +51,39 @@ contract SupeRareWrapper is Ownable  {
      * @dev See {IERC721-safeTransferFrom}.
      */
     function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public virtual override {
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
+        require(_isApprovedOrOwner(address(this), tokenId), "ERC721: transfer caller is not owner nor approved");
         _safeTransfer(from, to, tokenId, _data);
 
     }
 
-       /**
-     * @dev See {IERC721-isApprovedForAll}.
+
+  /**
+   * @dev Gets the approved address to take ownership of a given token ID
+   * @param _tokenId uint256 ID of the token to query the approval of
+   * @return address currently approved to take ownership of the given token ID
+   */
+  function getApproved(uint256 _tokenId) public view  returns (address) {
+    // return tokenApprovals[_tokenId];
+    return SupeOriginalAddr.approvedFor(_tokenId);
+  }
+
+/**
+     * @dev See {IERC721-safeTransferFrom}.
      */
-    function isApprovedForAll(address owner, address operator) public view virtual override returns (bool) {
-        return _operatorApprovals[owner][operator];
+    function safeTransferFrom(address from, address to, uint256 tokenId) public virtual override {
+        safeTransferFrom(from, to, tokenId, "");
     }
+
+    /**
+     * @dev See {IERC721-safeTransferFrom}.
+     */
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public virtual override {
+        require(_isApprovedOrOwner(msg.sender, tokenId), "ERC721: transfer caller is not owner nor approved");
+        _safeTransfer(from, to, tokenId, _data);
+    }
+
+
+
 
     /**
      * @dev Returns whether `spender` is allowed to manage `tokenId`.
@@ -61,22 +93,18 @@ contract SupeRareWrapper is Ownable  {
      * - `tokenId` must exist.
      */
     function _isApprovedOrOwner(address spender, uint256 tokenId) internal view virtual returns (bool) {
-        require(getApproved(tokenId) != address(0), "ERC721: operator query for nonexistent token");
-        address owner = ERC721Token.ownerOf(tokenId);
-        return (spender == owner || getApproved(tokenId) == spender || ERC721Token.isApprovedForAll(owner, spender));
+        // require(_exists(tokenId), "ERC721: operator query for nonexistent token");
+        require(SupeOriginalAddr.approvedFor(tokenId) != address(0),"ERC721: operator query for nonexistent token!" );
+
+        address owner = SupeOriginalAddr.ownerOf(tokenId);
+        // return (spender == owner || getApproved(tokenId) == spender || ERC721.isApprovedForAll(owner, spender));
+        return (spender == owner || getApproved(tokenId) == spender);
+
     }
 
-  /**
-   * @dev Gets the approved address to take ownership of a given token ID
-   * @param _tokenId uint256 ID of the token to query the approval of
-   * @return address currently approved to take ownership of the given token ID
-   */
-  function getApproved(uint256 _tokenId) public view virtual override returns (address) {
-    // return tokenApprovals[_tokenId];
-    return SupeOriginalAddr.approvedFor(_tokenId);
-  }
 
-        /**
+
+    /**
      * @dev Safely transfers `tokenId` token from `from` to `to`, checking first that contract recipients
      * are aware of the ERC721 protocol to prevent tokens from being forever locked.
      *
@@ -99,15 +127,62 @@ contract SupeRareWrapper is Ownable  {
         require(_checkOnERC721Received(from, to, tokenId, _data), "ERC721: transfer to non ERC721Receiver implementer");
     }
 
-    // /**
-    //  * @dev See {IERC721-safeTransferFrom}.
-    //  */
-    // function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public virtual override {
-    //     require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
-    //     _safeTransfer(from, to, tokenId, _data);
-    //     clearApprovalAndTransfer(from, to, tokenId);
+        /**
+     * @dev Transfers `tokenId` from `from` to `to`.
+     *  As opposed to {transferFrom}, this imposes no restrictions on msg.sender.
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     * - `tokenId` token must be owned by `from`.
+     *
+     * Emits a {Transfer} event.
+     */
+    function _transfer(address from, address to, uint256 tokenId) internal virtual {
+        // require(SupeOriginalAddr.ownerOf(tokenId) == from, "ERC721: transfer of token that is not own"); // internal owner
+        require(to != address(0), "ERC721: transfer to the zero address");
 
-    // }
+        // _beforeTokenTransfer(from, to, tokenId);
+        SupeOriginalAddr.transfer(to,tokenId);
+        // Clear approvals from the previous owner
+        // _approve(address(0), tokenId);
+
+        // _holderTokens[from].remove(tokenId);
+        // _holderTokens[to].add(tokenId);
+
+        // _tokenOwners.set(tokenId, to);
+
+        emit Transfer(from, to, tokenId);
+    }
+
+
+
+    /**
+     * @dev Internal function to invoke {IERC721Receiver-onERC721Received} on a target address.
+     * The call is not executed if the target address is not a contract.
+     *
+     * @param from address representing the previous owner of the given token ID
+     * @param to target address that will receive the tokens
+     * @param tokenId uint256 ID of the token to be transferred
+     * @param _data bytes optional data to send along with the call
+     * @return bool whether the call correctly returned the expected magic value
+     */
+    function _checkOnERC721Received(address from, address to, uint256 tokenId, bytes memory _data)
+        private returns (bool)
+    {
+        if (!to.isContract()) {
+            return true;
+        }
+        bytes memory returndata = to.functionCall(abi.encodeWithSelector(
+            IERC721Receiver(to).onERC721Received.selector,
+            _msgSender(),
+            from,
+            tokenId,
+            _data
+        ), "ERC721: transfer to non ERC721Receiver implementer");
+        bytes4 retval = abi.decode(returndata, (bytes4));
+        return (retval == _ERC721_RECEIVED);
+    }
 
 
 

2627fcd3957508010dcb2f7c84ab9d0a194a4c1f
cryptonomicon46 <sandman2145@gmail.com>
2023-01-25 21:31:54 -0800

    Changed the logic of the contract to hold a position

--- contracts/ISupeRareV2.sol
+++ contracts/ISupeRareV2.sol
@@ -1,30 +1,45 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity =0.7.6;
+pragma abicoder v2;
 // pragma solidity ^0.4.18;
 
 interface ISupeRareV2 {
 
+    /**
+     * @dev This captures the Depositer's V1 Position 
+     */
+    struct  Position {
+        address OrigOwner;
+        uint256 v1TokenID;
+        uint256 timeStamp;    
+    } 
+
 
       /**
-   * @notice deposit_V1 allows the owner of SupeRareV1 to deposit an NFT and get a minted representation in SupeRareV2
+   * @notice deposit allows the owner of SupeRareV1 to deposit an NFT and get a minted representation in SupeRareV2
    * @param _tokenId uint256 ID of the SupeRareV1 or original tokenId being deposited by owner
-   * @return Returns the V2 balance of the owner in the contract
+   * @return  true if it succeeds in it's operation
    * @dev emits a mint event
    */
-    function deposit_V1(uint256 _tokenId) external returns (uint256);
+    function deposit(uint256 _tokenId) external returns (bool);
 
 
 
     /**
-   * @notice Withdraw_V1 allows the owner of SupeRareV1 to withdraw the deposited V1 SupeRare NFT 
+   * @notice withdraw allows the owner of SupeRareV1 to withdraw the deposited V1 SupeRare NFT 
    * @param _tokenId uint256 ID of the SupeRareV1 which will be mapped to find the corresponding V2 NFT TokenID
-   * @return Returns the V2 balance of the owner in the contract
+   * @return  true if it succeeds in it's operation
    * @dev emits a burn event
    */
-    function Withdraw_V1(uint256 _tokenId) external returns (uint256);
+    function withdraw(uint256 _tokenId) external returns (bool);
 
-
+    /**
+   * @notice getOwnerPosition gets the owner's V1 position corresponding to teh v1TokenId
+   * @param v2TokenId Owner's current V2 TokenID
+   * @return  Position corresponding to the minted V2TokenID
+   */
+    function getOwnerPosition(uint256 v2TokenId) external returns (Position memory);
 
 
 }
--- contracts/SupeRareV2.sol
+++ contracts/SupeRareV2.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.7.6;
+pragma abicoder v2;
+
 import "./ISupeRare.sol";
 import "./ISupeRareV2.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
@@ -12,11 +14,26 @@ contract SupeRareV2 is Ownable, ERC721, ISupeRareV2  {
     address private owner_;
     bool private _stopped;
 
+
+   /**
+     * @dev V2 Token Minted that represents the Depositer's V1 position (address,uint256,uint256)
+     **/
+    mapping  (uint256 => Position) public  V2tokenToV1Position;
+
    /**
      * @dev Triggered when the contract is stopped or started
      **/
     event ToggleStartStop(bool);
 
+       /**
+     * @dev Triggered when a V1 token is deposited to mint a V2 token and position
+     **/
+    event PositionCreated(address indexed OrigOwner, uint256 indexed v1TokenID, uint256 indexed timeStamp);
+
+       /**
+     * @dev Triggered when a V2 token is burned to withdraw position
+     **/
+    event PositionDeleted(address indexed OrigOwner, uint256 indexed v1TokenID, uint256 indexed timeStamp);
   /**
      * @dev Maintain a mapping of the original owner address to V1 TokenID to minted V2 TokenID
      **/
@@ -31,54 +48,44 @@ contract SupeRareV2 is Ownable, ERC721, ISupeRareV2  {
         require(!_stopped,"DEPOSITS_DISABLED");
         _;
     }
+
     /**
-   * @notice deposit_V1 allows the owner of SupeRareV1 to deposit an NFT and get a minted representation in SupeRareV2
+  * @dev Guarantees msg.sender is owner of the given token
+  * @param _tokenId uint256 ID of the token to validate its ownership belongs to msg.sender
+  */
+  modifier onlyOwnerOf(uint256 _tokenId) {
+    require(ownerOf(_tokenId) == _msgSender());
+    _;
+  }
+    /**
+   * @notice deposit allows the owner of SupeRareV1 to deposit an NFT and get a minted representation in SupeRareV2
    * @param v1TokenId uint256 ID of the SupeRareV1 or original tokenId being deposited by owner
-   * @return Returns the V2 balance of the owner in the contract
-   * @dev emits a mint event, modifier allows stopping the deposits in case of an emergency
+   * @return bool true if the function succeeds in its operation
+   * @dev emits a PositionCreated event, creates the Position ,modifier allows stopping the deposits in case of an emergency
    */
-    function deposit_V1(uint256 v1TokenId) external virtual override StopDeposits returns (uint256) {
+    function deposit(uint256 v1TokenId) external StopDeposits virtual override returns (bool) {
         require(supe.ownerOf(v1TokenId) == _msgSender(),"SupeRareV2: Not the Owner of the SupeRareV1 Token!");
-        require(bytes(supe.tokenURI(v1TokenId)).length != 0,"SupeRareV2: Invalid V1 TokenURI!");
+        V2tokenToV1Position[v1TokenId] = Position(msg.sender,v1TokenId, block.timestamp);
         _mint(_msgSender(),v1TokenId);
-         _setTokenURI(v1TokenId, supe.tokenURI(v1TokenId));
-        return balanceOf(_msgSender());
+        emit PositionCreated(_msgSender(),v1TokenId,block.timestamp);
+        return true;
     }
 
     /**
-   * @notice Withdraw_V1 allows the owner of SupeRareV1 to withdraw the deposited V1 SupeRare NFT 
+   * @notice withdraw allows the owner of SupeRareV1 to withdraw the deposited V1 SupeRare NFT 
    * @param v1TokenId uint256 ID of the SupeRareV1 which will be mapped to find the corresponding V2 NFT TokenID
-   * @return Returns the V2 balance of the owner in the contract
-   * @dev emits a burn event
+   * @return bool true if the function succeeds in its operation
+   * @dev emits a burn event, deletes the corresponding Position 
    */
-    function Withdraw_V1(uint256 v1TokenId) external virtual override  returns (uint256) {
-        require(supe.ownerOf(v1TokenId) == _msgSender(),"SupeRareV2: Not the Owner of the SupeRareV1 Token!");
+    function withdraw(uint256 v1TokenId) external virtual override onlyOwnerOf(v1TokenId) returns (bool) {
+        require(supe.ownerOf(v1TokenId) == V2tokenToV1Position[v1TokenId].OrigOwner,"SupeRareV2: Not the Owner of the Original SupeRareV1 Token!");
         _burn(v1TokenId);
-        return balanceOf(_msgSender());
+        delete V2tokenToV1Position[v1TokenId];
+        emit PositionDeleted(_msgSender(),v1TokenId,block.timestamp);
+
+        return true;
     }
 
-     /**
-   * @notice safeTransferToEschrow allows the owner of SupeRareV1 to safe transfer to an eschrow
-   * @param from owner of the NFT Token
-   * @param to EOA or ERC721 compilant contract 
-   * @param tokenId of the NFT token to be transferred
-   * @param _data any data to be sent along with the transfer
-   * @dev emits Approval and transfer events
-   */
-    function safeTransferToEschrow(address from, address to, uint256 tokenId, bytes memory _data) external {
-        require(_exists(tokenId), "ERC721: operator query for nonexistent token");
-
-          address origOwner = ERC721.ownerOf(tokenId);
-        require(to != origOwner, "ERC721: approval to current owner");
-
-        require(_msgSender() == origOwner || ERC721.isApprovedForAll(origOwner, _msgSender()),
-            "ERC721: approve caller is not owner nor approved for all"
-        );
-
-        _approve(to, tokenId);
-        _safeTransfer(from, to, tokenId, _data);
-
-    }
     ///@notice contract_status, check if deposits are halted 
     ///@dev returns true= stopped || false == active
     function contract_status() external view returns (bool) {
@@ -94,6 +101,15 @@ contract SupeRareV2 is Ownable, ERC721, ISupeRareV2  {
     ///@notice getSupeRareAddress returns the address of the SupeRareV1 Contract
     function getSupeRareAddress() external view returns (address) {
         return OriginalSupeRareAddr_;
+    }
+
+        /**
+   * @notice getOwnerPosition gets the owner's V1 position corresponding to teh v1TokenId
+   * @param v2TokenId Owner's current V2 TokenID
+   * @return  DepositerPositionV1 corresponding to the minted V2TokenID
+   */
+    function getOwnerPosition(uint256 v2TokenId) external view virtual override returns (Position memory) {
+        return V2tokenToV1Position[v2TokenId];
     }
 
 }
